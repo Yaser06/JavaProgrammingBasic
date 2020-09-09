0d8ef8a4523575436b7c0f9b041f950aefb03c2d
Yaser <Yaser@DESKTOP-855JUGO>
2020-09-09 13:33:41 +0300

    CHAPTER 1

--- Bolum1Cözüm/src/Cozum5.java
+++ Bolum1Cözüm/src/Cozum5.java
@@ -3,6 +3,7 @@ public class Cozum5 {
 	public static void main(String[] args) {
 		double hesapla = ((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5);
 		System.out.println(hesapla);
+		System.out.println("Deneme");
 	}
 
 }
